09b40c4abdcdfeeccada95a50944982c55d1c9be
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-08-27 17:25:34 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './js/common';
+import './assets/css/main.css';
+import './assets/scss/main.scss';
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const init = () => {
+  // ReactDOM.render(
+  //     <Example/>,
+  //     document.querySelector(`#root`)
+  // );
+};
+
+init();
