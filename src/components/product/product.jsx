fa0831d45103f7b5c25e8a69b6561cc0022f9994
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-08-31 21:23:53 +0500

    Add proptypes, fix linter errors, fix card corners

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,8 +1,9 @@
-import React, {createRef} from "react";
-import {calculateBonus, declOfNum} from "../../utils/common/common";
-import {Declination, EventsMouse, Multiplicity} from "../../const";
+import React, {PureComponent, createRef} from 'react';
+import PropTypes from 'prop-types';
+import {calculateBonus, declOfNum} from '../../utils/common/common';
+import {Declination, EventsMouse, Multiplicity} from '../../const';
 
-class Product extends React.PureComponent {
+class Product extends PureComponent {
   constructor(props) {
     super(props);
 
@@ -18,51 +19,56 @@ class Product extends React.PureComponent {
     this._calculateAdditionalCaption = this._calculateAdditionalCaption.bind(this);
     this._changeDisplayingQuestion = this._changeDisplayingQuestion.bind(this);
   }
-  
+
   _handlerProductClick(evt) {
     evt.preventDefault();
+
+    if (this._ended) {
+      return;
+    }
+
     this.setState((prevState) => {
       const selected = !prevState.selected;
       this._checkboxRef.current.checked = selected;
       return {selected};
     });
   }
-  
+
   _changeDisplayingQuestion(evt) {
     if (!this.state.selected) {
-      return null;
+      return;
     }
     this.setState({displayQuestion: evt.type === EventsMouse.MOUSE_LEAVE});
   }
-  
+
   _calculateAdditionalCaption() {
     const {taste, description} = this.props.product;
     if (this._ended) {
       return `Печалька, ${taste} закончился`;
     }
-    
+
     if (this.state.selected) {
       return description;
     }
-    
+
     return (
       <>
         Чего сидишь? Порадуй котэ,
-        <a className="catalog__buy" href="#" onClick={this._handlerProductClick}> купи</a>
+        <a className='catalog__buy' href='#' onClick={this._handlerProductClick}> купи</a>
       </>
     );
   }
-  
+
   render() {
     const {id, caption, brand, taste, numberOfServings,
       weight, unit} = this.props.product;
     const {selected, displayQuestion} = this.state;
     const captionMain = displayQuestion ? `Котэ не одобряет?` : caption;
-    
+
     const declServing = declOfNum(numberOfServings, Declination.SERVING);
     const mouseCount = calculateBonus(numberOfServings, Multiplicity.SERVING);
     const declMouse = declOfNum(mouseCount, Declination.MOUSE);
-    
+
     return (
       <li className={`
           catalog__item
@@ -70,10 +76,10 @@ class Product extends React.PureComponent {
           ${selected ? ` catalog__item--selected` : ``}
         `}
       >
-        <input ref={this._checkboxRef} id={id} className="hidden" type="checkbox"/>
+        <input ref={this._checkboxRef} id={id} className='hidden' type='checkbox'/>
         <a
-          className="catalog__content"
-          href="#"
+          className='catalog__content'
+          href='#'
           onClick={this._handlerProductClick}
           onMouseLeave={this._changeDisplayingQuestion}
           onMouseEnter={this._changeDisplayingQuestion}
@@ -81,38 +87,52 @@ class Product extends React.PureComponent {
           <p className={`catalog__caption-main ${displayQuestion ? `catalog__caption-main--showed-question` : ``}`}>
             {captionMain}
           </p>
-          <h3 className="catalog__name-container">
-            <span className="catalog__name-brand">{brand}</span>
+          <h3 className='catalog__name-container'>
+            <span className='catalog__name-brand'>{brand}</span>
             <br/>
-            <span className="catalog__name-taste">{taste}</span>
+            <span className='catalog__name-taste'>{taste}</span>
           </h3>
-          <p className="catalog__spec">
-            <span className="catalog__quantity">{numberOfServings} </span>
+          <p className='catalog__spec'>
+            <span className='catalog__quantity'>{numberOfServings} </span>
             {declServing}
             <br/>
-            {mouseCount > 1 && <span className="catalog__quantity">{mouseCount} </span>}
+            {mouseCount > 1 && <span className='catalog__quantity'>{mouseCount} </span>}
             {declMouse} в подарок
             <br/>
             {numberOfServings >= Multiplicity.SATISFIED_CUSTOMER && `заказчик доволен`}
           </p>
-          <div className="catalog__picture-container">
+          <div className='catalog__picture-container'>
             <picture>
-              <source type="image/webp" srcSet="assets/img/cat_preview.webp"/>
-              <img className="catalog__picture" src="assets/img/cat_preview.png" width="260" height="275"
-                   alt={`${brand} ${taste}`}/>
+              <source type='image/webp' srcSet='assets/img/cat_preview.webp'/>
+              <img className='catalog__picture' src='assets/img/cat_preview.png' width='260' height='275'
+                alt={`${brand} ${taste}`}/>
             </picture>
           </div>
-          <div className="catalog__size-container">
-            <p className="catalog__size-value">{weight.toString().replace(`.`, `,`)}</p>
-            <p className="catalog__size-unit">{unit}</p>
+          <div className='catalog__size-container'>
+            <p className='catalog__size-value'>{weight.toString().replace(`.`, `,`)}</p>
+            <p className='catalog__size-unit'>{unit}</p>
           </div>
         </a>
-        <p className="catalog__caption-additional">
+        <p className='catalog__caption-additional'>
           {this._calculateAdditionalCaption()}
         </p>
       </li>
     );
   }
+}
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    brand: PropTypes.string.isRequired,
+    taste: PropTypes.string.isRequired,
+    numberOfServings: PropTypes.number.isRequired,
+    weight: PropTypes.number.isRequired,
+    unit: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    balance: PropTypes.number.isRequired,
+  }),
 };
 
 export default Product;
--- src/utils/common/common.js
+++ src/utils/common/common.js
@@ -1,27 +1,33 @@
 /**
  * Склоняет слова в зависимости от их количества
- * @param n {number} - количество
- * @param textForms {Array} - массив склонённых форм слова, например:
+ * @param {number} n - количество
+ * @param {Array} textForms - массив склонённых форм слова, например:
  * [`штука`, `штуки`, `штук`]
- * @returns {string} - элемент массива textForms
+ * @return {string} - элемент массива textForms
  */
 export const declOfNum = (n, textForms) => {
   n = Math.abs(n) % 100; const n1 = n % 10;
-  if (n > 10 && n < 20) { return textForms[2]; }
-  if (n1 > 1 && n1 < 5) { return textForms[1]; }
-  if (n1 === 1) { return textForms[0]; }
+  if (n > 10 && n < 20) {
+    return textForms[2];
+  }
+  if (n1 > 1 && n1 < 5) {
+    return textForms[1];
+  }
+  if (n1 === 1) {
+    return textForms[0];
+  }
   return textForms[2];
 };
 
 /**
  * Рассчитывает количество бонусов
- * @param currentCount (number) - текущее количество
- * @param multiplicity (number) - коэффициент начисления бонусов
- * @param minBonus (number) - если бонусов меньше этого числа,
+ * @param {number} currentCount  - текущее количество
+ * @param {number} multiplicity - коэффициент начисления бонусов
+ * @param {number} minBonus - если бонусов меньше этого числа,
  * то будет начислено столько бонусов
- * @returns {number} (number)
+ * @return {number}
  */
 export const calculateBonus = (currentCount, multiplicity = 1, minBonus = 1) => {
   const ratio = Math.floor(currentCount / multiplicity);
-  return  ratio > minBonus ? ratio : minBonus;
+  return ratio > minBonus ? ratio : minBonus;
 };
