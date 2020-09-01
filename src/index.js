9a6322848e35dc2d41bc90f7f5c51c68186e3a10
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-09-01 18:13:02 +0500

    Transfer js files, add favicon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./assets/scss/main.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import Catalog from "./components/catalog/catalog.jsx";
-import {catalogItems} from "./mocks/catalog-items";
+import Catalog from "./js/components/catalog/catalog.jsx";
+import {catalogItems} from "./js/mocks/catalog-items";
 
 const init = () => {
   ReactDOM.render(
